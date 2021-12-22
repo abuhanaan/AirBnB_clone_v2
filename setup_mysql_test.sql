5a232034d7f872d2b061da1fcee33d38e1e726f6
abuhanaan <adewunmi99@gmail.com>
2021-12-22 17:44:34 +0100

    Resolving conflict

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,18 @@
+<<<<<<< HEAD
+-- Creates the database hbnb_test_db with specified paramenters
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Creates user if doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+-- Sets password for user
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+-- Grants privileges to user on database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Grants select privileges to user on performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Flush privileges
+FLUSH PRIVILEGES;
+=======
 -- Script to create a MySQL server with the database hbnb_test_db.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
@@ -5,3 +20,4 @@ SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
 GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+>>>>>>> 08d37e6326007a64b50ce934687b5ed366f577ba
